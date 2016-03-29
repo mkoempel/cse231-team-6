f5f1b3bda2d97392c3bc1099d6d5fbb251c10e82
mkoempel <mkoempel@oakland.edu>
2016-03-29 10:41:53 -0400

    Create QueueControl.java

---
+++ QueueControl.java
@@ -0,0 +1,19 @@
+//This is the code from the monograph
+
+
+package edu.oakland.production;
+
+import java.util.*;
+
+public class QueueControl extends PriorityQueue{
+	
+	private String[] queueArray;
+	private int queueSize;
+	private int front, numberOfItems, rear = 0;
+	
+	
+	
+	
+	
+	constructor takes 3 arrays
+	3 return methods, one for each size
