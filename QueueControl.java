4a6884925a600bb4e2d1084ef1689eb77d271412
mkoempel <mkoempel@oakland.edu>
2016-03-29 10:47:39 -0400

    Update QueueControl.java

--- QueueControl.java
+++ QueueControl.java
@@ -10,10 +10,16 @@ public class QueueControl extends PriorityQueue{
 	private String[] queueArray;
 	private int queueSize;
 	private int front, numberOfItems, rear = 0;
+	private DisplayQueue display;
+	
+	public Queue Control(int size){
+		queueSize = size;
+		queueArray = new String
+	}
 	
 	
 	
-	
-	
+	/*
 	constructor takes 3 arrays
 	3 return methods, one for each size
+	*/
